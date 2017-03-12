bc4143f2157be22534ed9ed8204a691c280674de
ryhamz <ryhamz@gmail.com>
2017-03-12 18:23:18 +0800

    helper functions for grabbing protocol and filename. downloader functions for stfp, ftp, and http; still need to bring it all together and test

--- downloader.py
+++ downloader.py
@@ -1,5 +1,6 @@
 from ftplib import FTP
 import pysftp
+import requests
 
 
 def get_filename_from_url(url):
@@ -10,13 +11,50 @@ def get_protocol_from_url(url):
     return url.split(":")[0]
 
 
-def sftp_download(filename):
+def completeness_check(a, b):
+    print a, b
+    print 'goodbye'
+
+
+def sftp_download(url, path):
+    filename = get_filename_from_url(url)
     with pysftp.Connection('test.rebex.net', username='demo', password='password') as sftp:
         print sftp.exists('readme.txt')
-        sftp.get(filename)
+        sftp.get(filename,  localpath=path + filename,
+                 callback=completeness_check)
     return
+
+
+def ftp_download(filename, path, username=None, password=None):
+    ftp = FTP('test.rebex.net')
+    ftp.login(user=username, passwd=password)
+
+    local_file = open(path + filename, 'wb')
+    ftp.retrbinary('RETR ' + filename, local_file.write, 1024)
+
+    # Clean up
+    ftp.quit()
+    local_file.close()
+
+
+def http_download(url, path):
+    filename = get_filename_from_url(url)
+    r = requests.get(url)
+    with open(path + filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
+    return
+
+protocol_dispatch = {
+    "http": http_download,
+    "ftp": ftp_download,
+    "sftp": sftp_download
+}
 
 print get_filename_from_url("http://my.file.com/file")
 print get_protocol_from_url("http://something.com/myfile")
 
-sftp_download("readme.txt")
+sftp_download("test.rebex.net/readme.txt", "/Users/ryhamz/Desktop/")
+#url = "http://github.com/kennethreitz/requests/tarball/master"
+#protocol_dispatch[get_protocol_from_url(url)](url, "/Users/ryhamz/Desktop/")
