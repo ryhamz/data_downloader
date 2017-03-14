6af0ac21c3cd889fcb8bd79651c2ea1fb5e983a9
ryhamz <ryhamz@gmail.com>
2017-03-14 21:06:10 +0800

    changed some default arguments, changed main

--- downloader.py
+++ downloader.py
@@ -18,14 +18,23 @@ def get_protocol_from_url(url):
 
 
 def get_address_from_url(url):
+    # Helper function that returns address
+    # form a url. Strips the protocol and filename.
     return url.split("://")[1].split('/')[0]
 
 
-def sftp_download(url, path):
+def remove_data(path, filename):
+    # Remove a file at the given path.
+    # Will be used to remove incomplete data after partial downloads
+    os.remove(path + filename)
+    return 0
+
+
+def sftp_download(url, path, username='demo', password='password'):
     filename = get_filename_from_url(url)
     address = get_address_from_url(url)
 
-    with pysftp.Connection(address, username='demo', password='password') as sftp:
+    with pysftp.Connection(address, username=username, password=password) as sftp:
         print sftp.exists('readme.txt')
         try:
             print "sftp output: "
@@ -34,47 +43,56 @@ def sftp_download(url, path):
         except IOError as e:
             print "I/O error({0}): {1}".format(e.errno, e.strerror)
             remove_data(path, filename)
-    return
+        except:
+            print "Unexpected error:", sys.exc_info()[0]
+            remove_data(path, filename)
+            return 1
+    return 0
 
 
-def ftp_download(url, path, username=None, password=None):
+def ftp_download(url, path, username='demo', password='password'):
     filename = get_filename_from_url(url)
     address = get_address_from_url(url)
+
     ftp = FTP(address)
     ftp.login(user=username, passwd=password)
 
     local_file = open(path + filename, 'wb')
-    ftp.retrbinary('RETR ' + filename, local_file.write, 1024)
+    try:
+        ftp.retrbinary('RETR ' + filename, local_file.write, 1024)
+    except:
+        print "Unexpected error:", sys.exc_info()[0]
+        remove_data(path, filename)
+        return 1
 
     # Clean up
     ftp.quit()
     local_file.close()
+    return 0
 
 
 def http_download(url, path):
     filename = get_filename_from_url(url)
+
     r = requests.get(url, stream=True)
     content_length = r.headers['Content-Length']
     total = 0
-    with open(path + filename, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=1024):
-            if chunk:  # filter out keep-alive new chunks
-                total += len(chunk)
-                f.write(chunk)
-                print sys.getsizeof(f)
+    try:
+        with open(path + filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024):
+                if chunk:  # filter out keep-alive new chunks
+                    total += len(chunk)
+                    f.write(chunk)
+                    print sys.getsizeof(f)
+    except:
+        print "Unexpected error:", sys.exc_info()[0]
+        remove_data(path, filename)
+        return 1
     print total, content_length
     print total == int(content_length)
     if not total == int(content_length):
         remove_data(path, filename)
-    remove_data(path, filename)
-    return
-
-# Remove a file at the given path.
-# Will be used to remove incomplete data after partial downloads
-
-
-def remove_data(path, filename):
-    os.remove(path + filename)
+    return 0
 
 
 # Dispatch table containing our supported protocols as keys
@@ -86,11 +104,13 @@ protocol_dispatch = {
 }
 
 if __name__ == "__main__":
-    print get_filename_from_url("http://my.file.com/file")
-    print get_protocol_from_url("http://something.com/myfile")
+    # print get_filename_from_url("http://my.file.com/file")
+    # print get_protocol_from_url("http://something.com/myfile")
 
-    ftp_download("sftp://test.rebex.net/readme.txt",
-                 "/Users/ryhamz/Desktop/", "demo", "password")
+    # ftp_download("sftp://test.rebex.net/readme.txt",
+    #            "/Users/ryhamz/Desktop/", "demo", "password")
     # url = "http://github.com/kennethreitz/requests/tarball/master"
-    # protocol_dispatch[get_protocol_from_url(url)](
-    #    url, "/Users/ryhamz/Desktop/")
+    url = sys.argv[1]
+    output_directory = sys.argv[2]
+    protocol_dispatch[get_protocol_from_url(url)](
+        url, output_directory)
