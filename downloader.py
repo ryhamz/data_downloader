ac7146f37f3132e1781dea83225c35b50e072f08
ryhamz <ryhamz@gmail.com>
2017-03-14 19:37:25 +0800

    removed some hardcoding, unit tests are in\!

--- downloader.py
+++ downloader.py
@@ -2,32 +2,45 @@ from ftplib import FTP
 import pysftp
 import requests
 import sys
+import os
 
 
 def get_filename_from_url(url):
+    # Helper function that returns filename
+    # from a given url
     return url.split("/")[-1]
 
 
 def get_protocol_from_url(url):
+    # Helper function that returns the protocol
+    # from a given url.
     return url.split(":")[0]
 
 
-def completeness_check(a, b):
-    print a, b
-    print 'goodbye'
+def get_address_from_url(url):
+    return url.split("://")[1].split('/')[0]
 
 
 def sftp_download(url, path):
     filename = get_filename_from_url(url)
-    with pysftp.Connection('test.rebex.net', username='demo', password='password') as sftp:
+    address = get_address_from_url(url)
+
+    with pysftp.Connection(address, username='demo', password='password') as sftp:
         print sftp.exists('readme.txt')
-        sftp.get(filename,  localpath=path + filename,
-                 callback=completeness_check)
+        try:
+            print "sftp output: "
+            print sftp.get(filename,  localpath=path + filename,)
+            print "end sftp output"
+        except IOError as e:
+            print "I/O error({0}): {1}".format(e.errno, e.strerror)
+            remove_data(path, filename)
     return
 
 
-def ftp_download(filename, path, username=None, password=None):
-    ftp = FTP('test.rebex.net')
+def ftp_download(url, path, username=None, password=None):
+    filename = get_filename_from_url(url)
+    address = get_address_from_url(url)
+    ftp = FTP(address)
     ftp.login(user=username, passwd=password)
 
     local_file = open(path + filename, 'wb')
@@ -51,17 +64,33 @@ def http_download(url, path):
                 print sys.getsizeof(f)
     print total, content_length
     print total == int(content_length)
+    if not total == int(content_length):
+        remove_data(path, filename)
+    remove_data(path, filename)
     return
 
+# Remove a file at the given path.
+# Will be used to remove incomplete data after partial downloads
+
+
+def remove_data(path, filename):
+    os.remove(path + filename)
+
+
+# Dispatch table containing our supported protocols as keys
+# and their handlers as values
 protocol_dispatch = {
     "http": http_download,
     "ftp": ftp_download,
     "sftp": sftp_download
 }
 
-print get_filename_from_url("http://my.file.com/file")
-print get_protocol_from_url("http://something.com/myfile")
+if __name__ == "__main__":
+    print get_filename_from_url("http://my.file.com/file")
+    print get_protocol_from_url("http://something.com/myfile")
 
-#sftp_download("test.rebex.net/readme.txt", "/Users/ryhamz/Desktop/")
-url = "http://github.com/kennethreitz/requests/tarball/master"
-protocol_dispatch[get_protocol_from_url(url)](url, "/Users/ryhamz/Desktop/")
+    ftp_download("sftp://test.rebex.net/readme.txt",
+                 "/Users/ryhamz/Desktop/", "demo", "password")
+    # url = "http://github.com/kennethreitz/requests/tarball/master"
+    # protocol_dispatch[get_protocol_from_url(url)](
+    #    url, "/Users/ryhamz/Desktop/")
---
+++ tests.py
@@ -0,0 +1,33 @@
+import unittest
+import downloader
+
+
+class get_protocol_from_url_tests(unittest.TestCase):
+
+    def test_http_1(self):
+        res = downloader.get_protocol_from_url("http://my.path.to/thisfile")
+        self.assertEqual(res, "http")
+
+    def test_http_2(self):
+        res = downloader.get_protocol_from_url("http://my.path.to/thisfile")
+        self.assertNotEqual(res, "ftp")
+
+
+class get_filename_from_url_tests(unittest.TestCase):
+
+    def test_filename_1(self):
+        res = downloader.get_filename_from_url(
+            "http://my.path.to/thisfile.dat")
+        self.assertEqual(res, "thisfile.dat")
+
+    def test_filename_2(self):
+        res = downloader.get_filename_from_url(
+            "ftp://my.path.to/otherfile.txt")
+        self.assertNotEqual(res, "ftp")
+
+
+def main():
+    unittest.main()
+
+if __name__ == "__main__":
+    main()
